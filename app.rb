3fee05c3d6517c908c07e3692e463aa4ac3edc85
shihanliang <sliang1@ChicagoBooth.edu>
2025-04-21 05:25:59 +0000

    code

--- app.rb
+++ app.rb
@@ -5,17 +5,19 @@ get("/") do
   redirect("/add")
 end
 
-get("/ad") do
+# ADDITION
+get("/add") do
   erb(:add_form)
 end
 
 get("/wizard_add") do
   @first_num = params.fetch("first_num").to_f
-
+  @second_num = params.fetch("second_num").to_f
   @result = @first_num + @second_num
   erb(:add_result)
 end
 
+# SUBTRACTION
 get("/subtract") do
   erb(:sub_form)
 end
@@ -23,22 +25,30 @@ end
 get("/wizard_subtract") do
   @first_num = params.fetch("first_num").to_f
   @second_num = params.fetch("second_num").to_f
-
+  @result = @first_num - @second_num
   erb(:sub_result)
 end
 
+# MULTIPLICATION
 get("/multiply") do
   erb(:multiplication_form)
 end
 
-get("/wizard multiply") do
+get("/wizard_multiply") do
+  @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
+  @result = @first_num * @second_num
   erb(:mult_result)
 end
 
+# DIVISION
 get("/divide") do
   erb(:div_form)
 end
 
 get("/wizard_divide") do
+  @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
+  @result = @first_num / @second_num
   erb(:div_result)
 end
